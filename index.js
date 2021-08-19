dc0e1b420dd4980cdeec87a3365d03d0305ee17e
Shamil Qurbonov <qurbonovshamil@gmail.com>
2021-08-19 12:32:45 +0700

    Added nes socket example

---
+++ index.js
@@ -0,0 +1,22 @@
+const Nes = require('@hapi/nes');
+const wsEvents = { // socketEvents
+    state: '/FUNDRAISE_STATE',
+    swapped: '/FUNDRAISE_SWAPPED',
+    claim: '/FUNDRAISE_CLAIM',
+    token: '/STAKING_TOKEN',
+};
+
+// const client = new Nes.Client('wss://staging-samurai.cyberfi.tech/api');
+// const client = new Nes.Client('wss://develop-samurai.cyberfi.tech/api');
+const client = new Nes.Client('ws://192.168.88.67:8080');
+const start = async () => {
+    await client.connect();
+
+    for (const e of Object.values(wsEvents)) {
+        client.subscribe(e, (data) => {
+            console.log(e, ' | ', data)
+        })
+    }
+};
+
+start();
